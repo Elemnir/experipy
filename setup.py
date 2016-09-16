816cfd925c72b642f364a3b3b62a4d34116c359d
Elemnir <ahoward0920@gmail.com>
2016-09-15 20:39:15 -0400

    Added setup script and LICENSE

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='experipy',
+    description='A framework for writing and running Computational Science experiments',
+    author='Adam Howard',
+    author_email='ahoward0920@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+)
