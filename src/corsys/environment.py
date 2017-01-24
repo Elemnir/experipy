9c3dc245d6e80335d7fc197a4e2e46ef5b00504a
Elemnir <ahoward0920@gmail.com>
2017-01-24 13:08:22 -0500

    text patching

--- src/corsys/environment.py
+++ src/corsys/environment.py
@@ -3,11 +3,11 @@ from experipy.utils import Namespace
 
 projectdir = getenv("XPS_PROJDIR", "")
 if not projectdir:
-    projectdir = raw_input("CORSYS_PROJDIR not set, provide the project directory path\n>>> ")
+    projectdir = raw_input("XPS_PROJDIR not set, provide the project directory path\n>>> ")
 
 resultdir = getenv("XPS_VOLDIR", "")
 if not resultdir:
-    resultdir = raw_input("CORSYS_RESDIR not set, provide the result directory path\n>>> ")
+    resultdir = raw_input("XPS_VOLDIR not set, provide the result directory path\n>>> ")
 
 Env = Namespace(
     projdir = projectdir,
