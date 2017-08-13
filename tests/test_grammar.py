75239fb54d7358a4cadae592415fd88e95e25cb9
Elemnir <ahoward0920@gmail.com>
2017-08-12 22:19:15 -0400

    Adding some actual tests

---
+++ tests/test_grammar.py
@@ -0,0 +1,15 @@
+import unittest
+
+from experipy.grammar import Executable, Wrapper, Pipeline, Group
+
+class TestExecutable(unittest.TestCase):
+    def test_render(self):
+        exe = Executable("cat", ["1.txt", "2.txt"])
+        self.assertEqual(str(exe), "cat 1.txt 2.txt")
+
+    def test_output(self):
+        exe = Executable("echo", 
+            ['"Hello World"', "> test.txt"], 
+            outputs=["test.txt"]
+        )
+        self.assertEqual(list(exe.outputs()),["test.txt"])
